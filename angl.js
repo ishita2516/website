4059c77ef0c3c45aaa9d7d6963799e39fabe3a5c
Ishita <ishitasrivastavalko@gmail.com>
2020-12-11 10:00:38 +0530

    Update angl.js

--- angl.js
+++ angl.js
@@ -3,14 +3,14 @@ var app = angular.module("simpleApp", []);
 
 function simpleController() {
     this.collection = [{name: "america",artist:"Bill Eillie", price: 75650,},
-        {artname: "australia",artist:"Tate McRae", price: 15800},
-        {artname: "singapore",artist:"Taylor mike", price: 67850},
-        {artname: "canada",artist:"delwin", price: 81700},
-        {artname: "london",artist:"mathew", price: 11680},
-        {artname: "mauritius",artist:"Justin Blake", price: 55820}];
+        {name: "australia",artist:"Tate McRae", price: 15800},
+        {name: "singapore",artist:"Taylor mike", price: 67850},
+        {name: "canada",artist:"delwin", price: 81700},
+        {name: "london",artist:"mathew", price: 11680},
+        {name: "mauritius",artist:"Justin Blake", price: 55820}];
 
     this.limit = 9;
-    this.sortProperty = 'artname';
+    this.sortProperty = 'name';
     this.reverseSort = false;
 }
 
